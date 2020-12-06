12a9b99dbce2487bd2e4a2a40ffd9d1651e205b4
Amy Legge <amylegge@Amys-MacBook-Pro.local>
2020-12-06 18:22:05 -0500

    update to index.js to resolve heroku error

--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,8 @@ const router = require("express").Router();
 const dashboardRoutes = require('./dashboard-routes');
 const homeRoutes = require('./home-routes');
 
+router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
-router.use('/home', homeRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
