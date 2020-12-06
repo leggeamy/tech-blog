3c7c20cc9668b6e58b0b2ab1db509ef600c25750
Amy Legge <amylegge@Amys-MacBook-Pro.local>
2020-12-06 18:29:32 -0500

    index.js update testing reversal

--- controllers/index.js
+++ controllers/index.js
@@ -3,8 +3,11 @@ const router = require("express").Router();
 const dashboardRoutes = require('./dashboard-routes');
 const homeRoutes = require('./home-routes');
 
+// router.use('/dashboard', dashboardRoutes);
+// router.use('/home', homeRoutes);
+
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
-router.use('/api', apiRoutes);
+// router.use('/api', apiRoutes);
 
 module.exports = router;
